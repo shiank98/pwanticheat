0123d36114743765ddd8a739b0be700f702e6924
shiank98 <wesmontpaglomutan@gmail.com>
2019-11-02 21:55:39 +0800

    Add files via upload

---
+++ Anticheat/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Imports System
+Imports System.Diagnostics
+Imports System.ComponentModel
+Public Class PWReforged
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If System.Diagnostics.Process.
+             GetProcessesByName("AIRobot").Length > 0 Then
+            For Each Process In System.Diagnostics.Process.GetProcessesByName("elementclient")
+                Process.Kill()
+            Next
+        End If
+        If System.Diagnostics.Process.
+     GetProcessesByName("K25").Length > 0 Then
+            For Each Process In System.Diagnostics.Process.GetProcessesByName("elementclient")
+                Process.Kill()
+            Next
+        End If
+    End Sub
+End Class
---
+++ Anticheat/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("pwanticheat")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Anticheat")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("09d5e5e5-0088-44b9-98ac-040d8d9f00b2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
